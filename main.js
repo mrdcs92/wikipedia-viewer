554eb4e638b8ac56ad583ecfad10d6afdbfd550e
mrdcs92 <mrdcs92@gmail.com>
2017-09-07 00:43:39 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+  $("#submit").click(function(){
+    var searchTerm = $("#searchTerm").val();
+    if (searchTerm) {
+      var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+    $.ajax({
+      url: url,
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      success: function(data, status, jqXHR) {
+        $("#entryField").removeClass("entryField");
+        $("#entries").html("");
+        for(var i = 0; i < data[1].length; i++) {
+          $("#entries").append("<div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div>");
+        }
+      }
+    });
+    }
+  });
+});
